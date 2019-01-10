81cdcefc237faf3972b9856cd8f2eea18a4d3b8c
PascalCoinFreePool <pascalcoinfreepool@gmail.com>
2019-01-10 20:35:10 +0000

    frontend: change notification location

--- website_example/js/common.js
+++ website_example/js/common.js
@@ -420,7 +420,7 @@ toastr.options = {
     "debug": false,
     "newestOnTop": false,
     "progressBar": false,
-    "positionClass": "toast-bottom-center",
+    "positionClass": "toast-top-right",
     "preventDuplicates": false,
     "onclick": null,
     "showDuration": "300",
