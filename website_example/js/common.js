6a1e999148a52cd2a10669e353e04ac52508ad01
PascalCoinFreePool <pascalcoinfreepool@gmail.com>
2019-01-10 20:12:36 +0000

    frontend: block found notification

--- website_example/js/common.js
+++ website_example/js/common.js
@@ -414,3 +414,61 @@ function renderLangSelector() {
 $(".navbar-nav a:not(.dropdown-toggle), #accountSearch button").on("click", function(){
     $(".navbar-collapse").collapse("hide");
 });
+
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+function showToast(message="", title="") {
+    toastr.success(message, title);
+}
+
+var _totalBlocks = false;
+function checkForNewBlocks() {
+    if(!_totalBlocks) {
+        _totalBlocks = lastStats.pool.totalBlocks;
+        return;
+    }
+
+    var _lastBlock = false;
+
+    if(lastStats.pool.totalBlocks != _totalBlocks) {
+        _totalBlocks = lastStats.pool.totalBlocks;
+        var _lastBlockTimestamp = ":"+lastStats.pool.lastBlockFound.toString().slice(0, -3)+":";
+        for(var i = 0; i < lastStats.pool.blocks.length; i += 2) {
+            if(lastStats.pool.blocks[i].indexOf(_lastBlockTimestamp) !== -1) {
+                var parts = lastStats.pool.blocks[i].split(':');
+                _lastBlock = {
+                    height: parseInt(lastStats.pool.blocks[i+1]),
+                    worker: parts[0],
+                    hash: parts[1],
+                    time: parts[2],
+                    difficulty: parseInt(parts[3]),
+                    shares: parseInt(parts[4]),
+                    reward: (parts[5] / 10000).toFixed(4),
+                    orphaned: parts[6]
+                };
+                break;
+            }
+        }
+    }
+
+    if(_lastBlock !== false) {
+        var _effort = (100*_lastBlock.shares/_lastBlock.difficulty).toFixed(1)
+        var _height = parseInt(_lastBlock.height).toLocaleString();
+        showToast("Block at height "+_height+" found by " + _lastBlock.worker + " with "+_effort+"% effort", "Block Found!");
+    }
+}
